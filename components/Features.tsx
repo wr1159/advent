6369c699768f8d665ab1478114c8dfaa85a4886f
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-05-19 02:12:32 +0800

    Refactor code into react components

---
+++ components/Features.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FaPen, FaUser } from 'react-icons/fa';
+import { AiFillPieChart } from 'react-icons/ai';
+import { BsBoxArrowInDownRight } from 'react-icons/bs';
+import Card from './Card';
+
+const FeaturesComponent: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-bold text-black lg:text-7xl">
+        Features
+      </h1>
+      <div className="my-12 flex flex-col items-center justify-center space-y-10">
+        <div className="flex flex-row space-x-10">
+          <Card
+            title="Customisable"
+            body="Customise the event registration page as you wish!"
+            icon={<FaPen size={42} />}
+          />
+          <Card
+            title="User-First"
+            body="You are placed first into our designs. We strive for a simple and intuitive user experience."
+            icon={<FaUser size={42} />}
+          />
+        </div>
+        <div className="flex flex-row space-x-10">
+          <Card
+            title="Data Dashboard"
+            body="View the collected data at a single glance."
+            icon={<AiFillPieChart size={42} />}
+          />
+          <Card
+            title="Frictionless"
+            body="Seamless integration with your website through one copy button."
+            icon={<BsBoxArrowInDownRight size={42} />}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturesComponent;
