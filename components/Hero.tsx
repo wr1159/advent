54d070a6f20cf773e779cc67031f7aa9686a2181
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-05-19 02:09:04 +0800

    Tweak hero vertical margin values

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ interface HeroProps {
 }
 const Hero: React.FC<HeroProps> = ({ largeText, children }) => {
   return (
-    <div className="mt-16 flex h-screen flex-col items-center px-20 lg:mt-64 ">
+    <div className="my-32 flex h-3/4 flex-col items-center px-20 lg:mt-64 ">
       <h1 className="pb-12 text-center text-4xl font-bold text-black lg:text-8xl">
         {largeText}
       </h1>
