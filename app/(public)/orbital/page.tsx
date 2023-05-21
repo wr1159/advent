01ea8a186c99e36159dcb556401e1eb38bf07752
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-05-22 01:52:22 +0800

    Add Orbital Page (#8)
    * Updated README.md
    
    * Installed tailwindcss-prettier
    
    * Fix tailwindcss formatting
    
    * Install tailwind-merge
    
    * Add button component
    
    * Add mobile consideration for buttons
    
    * Add hero component
    
    * Install react-icons
    
    * Tweak hero vertical margin values
    
    * Add cards component and add features section
    
    * Refactor code into react components
    
    * Change Navbar to be more flexible
    
    * Revert "Change Navbar to be more flexible"
    
    This reverts commit 877874cc78cd3f581439fce44eb15b6f9b636067.
    
    * Change Navbar to be more flexible
    
    * Add routing to all pages
    
    * Change secondary colour
    
    * Add Orbital Components
    
    * Allow Card Props to be public
    
    * Add orbital page
    
    * Change slug to use hyphens
    
    * Add liftoff proposal and poster
    
    * Add key to navbar
    
    * Remove redundant page.tsx

--- app/(public)/orbital/page.tsx
+++ app/(public)/orbital/page.tsx
@@ -1,7 +1,57 @@
+import Hero from '@/components/Hero';
+import OrbitalRow from '@/components/OrbitalRow';
+
+const liftoff = [
+  {
+    href: '/src/liftoff-proposal.pdf',
+    title: 'Liftoff Proposal',
+    body: 'First iteration of our proposal.'
+  },
+  {
+    href: '/src/liftoff-poster.jpg',
+    title: 'Liftoff Poster',
+    body: 'First iteration of our poster.'
+  },
+  {
+    href: 'https://youtu.be/iHqpLQbVtEk',
+    title: 'Liftoff Video',
+    body: 'First video, credits to Vivaldi for the sick soundtrack.'
+  }
+];
+
 export default function Orbital() {
   return (
-    <div>
-      <h1>Orbital Page</h1>
+    <div className="my-8 flex w-full flex-col items-center">
+      <div className="mb-4 flex w-full flex-col items-center space-y-6 text-xl">
+        <h1 className="text-3xl text-accent lg:text-5xl">
+          <span className="advent text-black">Advent</span> - NUS Orbital 2023
+          Project
+        </h1>
+        <h2 className="text text-2xl text-accent">
+          This page contains all our documents submitted for Orbital 2023.
+        </h2>
+      </div>
+      <div className="my-8 w-full border border-accent"></div>
+
+      <div className="mx-auto mb-8 grid grid-cols-1 justify-center gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="text-center">
+          <h3 className="text-primary">Team name</h3>
+          <h2 className="text-2xl">
+            <span className="advent">Advent</span>
+          </h2>
+        </div>
+
+        <div className="text-center">
+          <h3 className="text-primary">Team number</h3>
+          <h2 className="text-2xl">5877</h2>
+        </div>
+        <div className="text-center">
+          <h3 className="text-primary">Achievement Level</h3>
+          <h2 className="text-2xl">Artemis</h2>
+        </div>
+      </div>
+      <h2 className="my-4 text-2xl text-accent">Liftoff</h2>
+      <OrbitalRow linkCards={liftoff} />
     </div>
   );
 }
