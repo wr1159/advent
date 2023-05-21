36de55caa6f5636cb7bb1c86bbc452543c885d0c
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-05-21 23:07:37 +0800

    Fix features in desktop layout

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,7 +8,7 @@ export interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, body, icon }) => {
   return (
-    <div className="mx-auto flex h-52 w-72 flex-col rounded bg-secondary p-4 md:h-60 md:w-96 md:p-8">
+    <div className="w-76 mx-8 flex h-48 flex-col rounded-md bg-secondary p-8 md:h-60 md:w-96">
       {icon && <div className="mb-2">{icon}</div>}
       <h3 className="mb-2 text-base font-semibold text-accent md:text-2xl">
         {title}
--- components/Features.tsx
+++ components/Features.tsx
@@ -10,27 +10,29 @@ const FeaturesComponent: React.FC = () => {
       <h1 className="text-center text-4xl font-bold text-black lg:text-7xl">
         Features
       </h1>
-      <div className="my-12 grid grid-cols-1 justify-center gap-6 md:grid-cols-2">
-        <Card
-          title="Customisable"
-          body="Customise the event registration page as you wish!"
-          icon={<FaPen size={42} />}
-        />
-        <Card
-          title="User-First"
-          body="You are placed first into our designs. We strive for a simple and intuitive user experience."
-          icon={<FaUser size={42} />}
-        />
-        <Card
-          title="Data Dashboard"
-          body="View the collected data at a single glance."
-          icon={<AiFillPieChart size={42} />}
-        />
-        <Card
-          title="Frictionless"
-          body="Seamless integration with your website through one copy button."
-          icon={<BsBoxArrowInDownRight size={42} />}
-        />
+      <div className="flex flex-wrap justify-center">
+        <div className="my-12 grid grid-cols-1 gap-8 md:grid-cols-2">
+          <Card
+            title="Customisable"
+            body="Customise the event registration page as you wish!"
+            icon={<FaPen size={42} />}
+          />
+          <Card
+            title="User-First"
+            body="You are placed first into our designs. We strive for a simple and intuitive user experience."
+            icon={<FaUser size={42} />}
+          />
+          <Card
+            title="Data Dashboard"
+            body="View the collected data at a single glance."
+            icon={<AiFillPieChart size={42} />}
+          />
+          <Card
+            title="Frictionless"
+            body="Seamless integration with your website through one copy button."
+            icon={<BsBoxArrowInDownRight size={42} />}
+          />
+        </div>
       </div>
     </div>
   );
