91c4e1e19a9df675dbdc46d0a09daf281ea03f3a
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-05-24 21:13:08 +0800

    Updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './'
+});
+
+const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/public'],
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    'components/**/*.{ts,tsx}',
+    'app/**/*.{ts,tsx}',
+    'utils/**/*.{ts,tsx}'
+  ],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
 };
+module.exports = createJestConfig(customJestConfig);
