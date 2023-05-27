d026579cf0caa315f8f76c4c3d07f5f07c6d9de1
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-05-28 00:28:00 +0800

    Add Charts in Statistics Page (#33)
    * Install chart js
    
    * Add Linechart
    
    * Add LineCharts
    
    * Add BarChart
    
    * Made ChartContainer extensible

--- app/(dashboard)/dashboard/statistics/page.tsx
+++ app/(dashboard)/dashboard/statistics/page.tsx
@@ -1,7 +1,77 @@
+import LineChart from '@/components/Charts/LineChart';
+import ChartContainer from '@/components/Chart';
+import LineChartContainer from '@/components/Chart';
+
+export interface mockAttendant {
+  name: string;
+  signUpDate: Date;
+  attendingDay: number;
+  country: string;
+}
+
 export default function Statistics() {
+  const attendants: mockAttendant[] = [
+    {
+      name: 'John',
+      signUpDate: new Date('2023-05-24'),
+      attendingDay: 1,
+      country: 'Singapore'
+    },
+    {
+      name: 'Emma',
+      signUpDate: new Date('2023-05-24'),
+      attendingDay: 1,
+      country: 'Malaysia'
+    },
+    {
+      name: 'Liam',
+      signUpDate: new Date('2023-05-25'),
+      attendingDay: 1,
+      country: 'Indonesia'
+    },
+    {
+      name: 'Olivia',
+      signUpDate: new Date('2023-05-25'),
+      attendingDay: 3,
+      country: 'Singapore'
+    },
+    {
+      name: 'Noah',
+      signUpDate: new Date('2023-05-25'),
+      attendingDay: 2,
+      country: 'Malaysia'
+    },
+    {
+      name: 'Ava',
+      signUpDate: new Date('2023-05-26'),
+      attendingDay: 3,
+      country: 'Indonesia'
+    }
+  ];
+
   return (
-    <div>
-      <h1>Statistics Page</h1>
+    <div className="m-10 flex flex-col space-y-5">
+      <h1 className="text-3xl font-semibold">Data at a glance</h1>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+        <ChartContainer
+          heading="Total Registrants Trend"
+          subheading="Number of Registrants"
+          attendants={attendants}
+          param="signUpDate"
+          label="Sign-ups per Day"
+          colSpan={1}
+          chartType="line"
+        />
+        <ChartContainer
+          heading="Total Attendees Trend"
+          subheading="Number of Attendees"
+          attendants={attendants}
+          param="attendingDay"
+          label="Attendees on the day"
+          colSpan={1}
+          chartType="bar"
+        />
+      </div>
     </div>
   );
 }
