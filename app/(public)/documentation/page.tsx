c13e392eda0e26b227e5a42644c9aaa42c6139e7
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-05-29 03:33:45 +0800

    Add documentation  (#39)
    * Setup MDX
    
    * Add tailwindcss typography
    
    * Enable typography plugin
    
    * Remove experimental feature
    
    * Add MDX component to documentation and milestone 1 README

--- app/(public)/documentation/page.tsx
+++ app/(public)/documentation/page.tsx
@@ -1,7 +1,8 @@
+import MilestoneOne from '../../../components/MDX/MilestoneOne.mdx';
 export default function Documentation() {
   return (
-    <div>
-      <h1>Documentation Page</h1>
-    </div>
+    <article className="prose min-w-full p-10">
+      <MilestoneOne />
+    </article>
   );
 }
---
+++ app/(public)/orbital/milestone-one/page.tsx
@@ -0,0 +1,9 @@
+import MilestoneOne from '../../../../components/MDX/MilestoneOne.mdx';
+
+export default function MilestoneOnePage() {
+  return (
+    <article className="prose min-w-full p-10">
+      <MilestoneOne />
+    </article>
+  );
+}
