640c581b472c4a2fb88a44574e98bfe61e0c4074
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-05-28 12:08:39 +0800

    Add userId into eventItem and getUser function to get userId

--- components/EventItem.tsx
+++ components/EventItem.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link';
 import { EventOperations } from './EventOperations';
+import getUserId from '@/utils/getUser';
 
 interface EventItemProps {
+  uid: string;
   event: {
     id: string;
     name: string;
     date: string;
   };
 }
-
-export function EventItem({ event }: EventItemProps) {
+export function EventItem({ uid, event }: EventItemProps) {
   return (
     <div className="flex items-center justify-between p-4">
       <div className="grid gap-1">
         <Link
-          href={`/editor/${event.id}`}
+          href={`/editor/${uid}/${event.id}`}
           className="font-semibold hover:underline"
         >
           {event.name}
