4353ac2a100f731752cb528730b185fb32fd85c4
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-06-09 10:27:53 +0800

    Add ProjectLog page

---
+++ app/(public)/orbital/project-log/page.tsx
@@ -0,0 +1,20 @@
+import WRPRojectLog from '../../../../components/MDX/WRProjectLog.mdx';
+import NathanProjectLog from '../../../../components/MDX/NathanProjectLog.mdx';
+
+export default function MilestoneOnePage() {
+  return (
+    <article className="prose flex min-w-full justify-start gap-x-6 p-10">
+      <div className="top-0">
+        <h2 className="text-center text-lg text-accent md:text-2xl">
+          Wei Rong
+        </h2>
+        <WRPRojectLog />
+      </div>
+      <div className="min-h-full border"></div>
+      <div className="top-0">
+        <h2 className="text-center text-lg text-accent md:text-2xl">Nathan</h2>
+        <NathanProjectLog />
+      </div>
+    </article>
+  );
+}
