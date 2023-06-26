3762c04dfc0fca835ce83a6e9ef5b63e050d9f95
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-06-26 13:22:19 +0700

    Add Orbital Pages

---
+++ app/(public)/orbital/milestone-two/page.tsx
@@ -0,0 +1,9 @@
+import MilestoneTwo from '../../../../components/MDX/MilestoneTwo.mdx';
+
+export default function MilestoneOnePage() {
+  return (
+    <article className="prose min-w-full p-10">
+      <MilestoneTwo />
+    </article>
+  );
+}
