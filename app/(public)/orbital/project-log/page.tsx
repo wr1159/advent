17ab5979df6795bc8e5ab3e4be6a0031f50a893a
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-06-28 21:25:32 +0700

    Change nathan project log + even width

--- app/(public)/orbital/project-log/page.tsx
+++ app/(public)/orbital/project-log/page.tsx
@@ -4,14 +4,14 @@ import NathanProjectLog from '../../../../components/MDX/NathanProjectLog.mdx';
 export default function MilestoneOnePage() {
   return (
     <article className="prose flex min-w-full justify-start gap-x-6 p-10">
-      <div className="top-0">
+      <div className="top-0 w-1/2">
         <h2 className="text-center text-lg text-accent md:text-2xl">
           Wei Rong
         </h2>
         <WRPRojectLog />
       </div>
       <div className="min-h-full border"></div>
-      <div className="top-0">
+      <div className="top-0 w-1/2">
         <h2 className="text-center text-lg text-accent md:text-2xl">Nathan</h2>
         <NathanProjectLog />
       </div>
