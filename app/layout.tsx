a6eb54fd05d5742b725e8c41b70a493015028f7e
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-07-20 17:41:42 +0700

    Add open-graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Advent',
+  title: 'Advent - Canva for Event Organisation',
   description:
-    'Advent - simplifying event management for organisers and participants'
+    'Say goodbye to inconsistent theming in the event registration flow and redirection to third party registration apps. Welcome a simple solution to integrate an event registration page to your website, customised to your liking.'
 };
 
 export default function RootLayout({
@@ -16,6 +16,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          property="og:title"
+          content="Advent - Canva for Event Organisation"
+        />
+        <meta
+          property="og:description"
+          content="Say goodbye to inconsistent theming in the event registration flow and redirection to third party registration apps. Welcome a simple solution to integrate an event registration page to your website, customised to your liking."
+        />
+        <meta property="og:image" content="/src/opengraph.png" />
+        <meta
+          property="og:image:alt"
+          content="Advent supports many features!"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
