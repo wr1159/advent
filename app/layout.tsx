ebcbfa48c3c645c3ccf9a84d3db2935ab82b03f2
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-07-20 20:16:12 +0700

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,39 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Advent',
-  description: 'Canva for Event Organisation'
+  description: 'Canva for Event Organisation',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://advent-beta.vercel.app',
+    title: 'Advent',
+    description: 'Canva for Event Organisation',
+    siteName: 'Advent',
+    images: [
+      {
+        url: '/src/opengraph.png',
+        width: 1200,
+        height: 630,
+        alt: 'Advent description'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Advent',
+    description: 'Canva for Event Organisation',
+    images: [
+      {
+        url: '/src/opengraph.png',
+        width: 1200,
+        height: 630,
+        alt: 'Advent description'
+      }
+    ]
+  },
+  icons: {
+    icon: '/src/icon.png'
+  }
 };
 
 export default function RootLayout({
@@ -16,18 +48,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta property="og:title" content="Advent" />
-        <meta
-          property="og:description"
-          content="Canva for Event Organisation"
-        />
         <meta property="og:image" content="/src/opengraph.png" />
         <meta
           property="og:image:alt"
           content="Advent supports many features!"
         />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
