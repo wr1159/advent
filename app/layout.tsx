11fcb58b5222e27738ed4eddb64cef91fe0cf7c3
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-07-21 12:25:39 +0800

    Opengraph support (#75)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,39 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Advent',
-  description:
-    'Advent - simplifying event management for organisers and participants'
+  description: 'Canva for Event Organisation',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://advent-beta.vercel.app',
+    title: 'Advent',
+    description: 'Canva for Event Organisation',
+    siteName: 'Advent',
+    images: [
+      {
+        url: '/src/opengraph.png',
+        width: 1200,
+        height: 630,
+        alt: 'Advent description'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Advent',
+    description: 'Canva for Event Organisation',
+    images: [
+      {
+        url: '/src/opengraph.png',
+        width: 1200,
+        height: 630,
+        alt: 'Advent description'
+      }
+    ]
+  },
+  icons: {
+    icon: '/src/icon.png'
+  }
 };
 
 export default function RootLayout({
@@ -16,6 +47,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="/src/opengraph.png" />
+        <meta
+          property="og:image:alt"
+          content="Advent supports many features!"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
